d550c272d6a06c7b994c439f0a0e1dc827182a2e
deep-deep-learning <keonyong_lee@ucsb.edu>
2022-04-06 19:56:45 +0000

    new log_prior for TT format

--- low_rank_tensor.py
+++ low_rank_tensor.py
@@ -1,9 +1,10 @@
+from cmath import log
 import tltorch
 import torch
 import torch.nn as nn
 from torch.distributions.half_cauchy import HalfCauchy
 from torch.distributions.normal import Normal
-from .distribution import LogUniform
+from tensor_fusion.distribution import LogUniform
 import numpy as np
 
 class LowRankTensor(nn.Module):
@@ -75,17 +76,18 @@ class CP(LowRankTensor):
 
     def get_log_prior(self):
 
+        # clamp to the domain of prior
         with torch.no_grad():
             self.rank_parameter[:] = self.rank_parameter.clamp(1e-30, 1e30)
         
-        # self.threshold(self.rank_parameter)
+        # the log prior of rank parameter
         log_prior = torch.sum(self.rank_parameter_prior_distribution.log_prob(self.rank_parameter))
         
-        # 0 mean normal distribution for the factors
-        factor_prior_distribution = Normal(0, self.rank_parameter)
-        for factor in self.tensor.factors:
-            log_prior = log_prior + factor_prior_distribution.log_prob(factor).sum(0).sum()
-        
+        for i in range(self.max_rank):
+            column_prior = Normal(0, self.rank_parameter[i])
+            for factor in self.tensor.factors:
+                log_prior += column_prior.log_prob(factor[:,i]).sum()
+
         return log_prior
     
     def estimate_rank(self):
@@ -99,51 +101,69 @@ class CP(LowRankTensor):
 
 class TT(LowRankTensor):
 
-    def __init__(self, in_features, out_features, max_rank, prior_type='log_uniform', eta=None, device=None, dtype=None):
+    def __init__(self, in_features, out_features, max_rank, 
+                 prior_type='log_uniform', eta=None, 
+                 device=None, dtype=None):
         '''
-        max_rank: an 'int' or a 'float' if indicating comprssion ratio
+        arguments:
+            in_features: an int, the input size
+            out_features: an int, the output size
+            max_rank: a float, if representing compression ratio; 
+                      an int, if representing actual tensor rank
+            prior_type: a str, the prior distribution of the rank parameter:
+                        'log_uniform' or 'half_cauchy'
+            eta: a float, the hyper-parameter if prior_type == 'half_cauchy'
         '''
 
         super().__init__(in_features, out_features, prior_type, eta, device, dtype)
 
+        # get tensorized dim for TT format
+        # ex) tensorized_shape = ((2, 4, 8), (4, 4, 8))
+        #     tensorized_dim = (2, 4, 8, 4, 4, 8)
         self.tensorized_dim = [*self.tensorized_shape[0], *self.tensorized_shape[1]]
-            
+        
+        # get tltorch TT tensor
+        # it has len(tensorized_dim) TT cores with the shapes of (rank[i], tensorized_dim[i], rank[i+1])
         self.tensor = tltorch.TTTensor.new(shape=self.tensorized_dim, rank=max_rank, device=device, dtype=dtype)
+        # set the max rank
         self.max_rank = self.tensor.rank
+        # set the target stddev for TT core initialization
         target_var = 1 / in_features
         factor_std = ((target_var / np.prod(self.tensor.rank)) ** (1 / self.tensor.order)) ** 0.5
-
+        # initialize the TT cores
         for factor in self.tensor.factors:
             nn.init.normal_(factor, 0, factor_std)
-
-        self.rank_parameter = nn.ParameterList([nn.Parameter(torch.rand((self.max_rank[1+i],), device=device, dtype=dtype)) \
+        # initialize the list of rank parameters
+        self.rank_parameters = nn.ParameterList([nn.Parameter(torch.rand((self.max_rank[1+i],), device=device, dtype=dtype)) \
             for i in range(len(self.tensorized_dim)-1)])
 
     def get_log_prior(self):
 
         log_prior = 0.0
-        for r in self.rank_parameter:
-            # clamp rank_param because <=0 is undefined 
+        for rank_parameter in self.rank_parameters:
             with torch.no_grad():
-                r[:] = r.clamp(1e-10, 1e10)
-            log_prior = log_prior + torch.sum(self.rank_parameter_prior_distribution.log_prob(r))
+                rank_parameter[:] = rank_parameter.clamp(1e-10, 1e10)
+            log_prior = log_prior + torch.sum(self.rank_parameter_prior_distribution.log_prob(rank_parameter))
 
-        
-        # 0 mean normal distribution for the factors
-        for i in range(len(self.rank_parameter)):
-            factor_prior_distribution = Normal(0, self.rank_parameter[i])
-            log_prior = log_prior + factor_prior_distribution.log_prob(self.tensor.factors[i]).sum((0,1)).sum()
-        
-        factor_prior_distribution = Normal(0, self.rank_parameter[-1])
-        log_prior = log_prior + factor_prior_distribution.log_prob(self.tensor.factors[-1]).sum((1,2)).sum()
+        for n, rank_parameter in enumerate(self.rank_parameters):
+            for i in range(rank_parameter.shape[0]):
+                slice_column_prior = Normal(0, rank_parameter[i])
+                slice_column = self.tensor.factors[n][:,:,i]
+                log_prior = log_prior + slice_column_prior.log_prob(slice_column).sum()
+
+        for i in range(rank_parameter.shape[0]):
+            slice_column_prior = Normal(0, rank_parameter[i])
+            slice_column = self.tensor.factors[-1][i,:,:]
+            log_prior = log_prior + slice_column_prior.log_prob(slice_column).sum()
 
         return log_prior
     
     def estimate_rank(self):
 
         rank = [1]
-        for factor in self.tensor.factors[:-1]:
+        for factor in self.tensor.factors[:-2]:
             rank.append(torch.sum(factor.var((0,1)) > 1e-4).item())
+        rank.append(torch.sum(self.tensor.factors[-1].var(1,2) > 1e-4).item())
         
         #rank.append(torch.sum(self.tensor.factors[-1].var((1,2) > 1e-5)))
         rank.append(1)
